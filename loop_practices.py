05cab161f2996acd0004bd36232ce617f0ae8b54
tugrul tuncel <tugrultuncel95@gmail.com>
2026-09-17 16:30:11 +0000

    added range, zip, enumerate text and practices.

--- loop_practices.py
+++ loop_practices.py
@@ -23,3 +23,50 @@ for word in words:
             break
     else:
         print(f"'{word}' has no vowels")
+
+print(range(3))
+
+for num in range(3):
+    print(num)
+
+for num in range(1, 5):
+    print(num)
+
+for num in range(0, 5):
+    print(num)
+
+for num in range(40, 0, -10):
+    print(num)
+
+for num in range(10,0,-1):
+    print(num)
+
+numbers = list(range(1,11,1))
+print(numbers)
+
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+index = 0
+order = 1
+
+for language in languages:
+    print(f'Order {order}: Index {index} and language {language}')
+    index += 1
+    order += 1
+
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+for index, language in enumerate(languages):
+    print(f'Index {index} and language {language}')
+
+developers = ['Naomi', 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
+
+print(list(zip(developers, ids)))
+
+developers = ['Naomi', 'Dario', 'Jessica', 'Tom']
+ids = [1, 2, 3, 4]
+
+for name, dev_id in zip(developers, ids):
+    print(f'Name: {name}')
+    print(f'ID: {dev_id}')
